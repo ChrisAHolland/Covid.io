e923855e5a255925e2ac5674480ec18cbe69fce8
Ben Austin <benjaminaustin@uvic.ca>
2020-11-15 09:52:50 -0800

    Add timed round scoring

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { time } = require("console");
 var express = require("express");
 var app = express();
 var server = require("http").Server(app);
@@ -10,11 +11,74 @@ var target = {
   y: Math.floor(Math.random() * 1100) + 50
 };
 
+// current round scores
 var scores = {
   doctor: 0,
   virus: 0
 };
 
+// rounds won
+var rounds = {
+  doctor: 0,
+  virus: 0
+};
+
+// game round time in seconds
+var countdown = 20;
+
+// timer object for round timer
+function Timer(fn, time) {
+  var timerObject = setInterval(fn, time);
+
+  // stop the timer
+  this.stop = function() {
+    if(timerObject) {
+      clearInterval(timerObject);
+      timerObject = null;
+    }
+    return this;
+  }
+
+  // start timer
+  this.start = function() {
+    if(!timerObject) {
+      this.stop();
+      timerObject = setInterval(fn, time);
+    }
+    return this;
+  }
+
+  // reset round timer
+  this.reset = function(){
+    countdown = 20;
+  }
+}
+
+var timer = new Timer(function() {
+  countdown--;
+
+  if(countdown < 0){
+    timer.stop();
+    
+    if(scores.doctor > scores.virus) {
+      rounds.doctor += 1;
+    } else if (scores.virus > scores.doctor) {
+      rounds.virus += 1;
+    } 
+
+    // reset scores and player sizes for new round
+    scores.doctor = 0;
+    scores.virus = 0;
+
+    // update players
+    io.emit("scoreUpdate", scores);
+    io.emit("roundUpdate", rounds);
+
+    timer.reset();
+    timer.start();
+  }
+  io.emit("clockUpdate", countdown);
+}, 1000);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -23,12 +87,12 @@ app.get("/", function (req, res) {
 });
 
 io.on("connection", function (socket) {
-  console.log("a user connected");
+  console.log("A user connected!");
   // create a new player and add it to our players object
   players[socket.id] = {
     rotation: 0,
-    x: Math.floor(Math.random() * 1500) + 50,
-    y: Math.floor(Math.random() * 1100) + 50,
+    x: Math.floor(Math.random() * 1525) + 25, 
+    y: Math.floor(Math.random() * 665) + 135, 
     player_height: 53,
     player_width: 53,
     playerId: socket.id,
@@ -40,15 +104,18 @@ io.on("connection", function (socket) {
   // send the target object to the new player
   socket.emit('targetLocation', target);
   
-  // send the current 
+  // send the current score
   socket.emit('scoreUpdate', scores);
+
+  // send the current rounds
+  socket.emit("roundUpdate", rounds);
 
   // update all other players of the new player
   socket.broadcast.emit("newPlayer", players[socket.id]);
 
   // when a player disconnects, remove them from our players object
   socket.on("disconnect", function () {
-    console.log("user disconnected");
+    console.log("A user disconnected!");
     // remove this player from our players object
     delete players[socket.id];
     // emit a message to all players to remove this player
@@ -72,8 +139,9 @@ io.on("connection", function (socket) {
     } else {
       scores.doctor += 1;
     }
-    target.x = Math.floor(Math.random() * 1500) + 50;
-    target.y = Math.floor(Math.random() * 1100) + 50;
+    target.x = Math.floor(Math.random() * 1525) + 25;
+    target.y = Math.floor(Math.random() * 665) + 135;
+    console.log("target x/y: " + target.x + "/" + target.y);
     io.emit('targetLocation', target);
     io.emit('scoreUpdate', scores);
   });
